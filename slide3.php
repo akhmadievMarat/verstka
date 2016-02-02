27bee8785708680d292e0529cd1672d34031af90
marat <akhmadiev1993@mail.ru>
2016-02-02 14:43:21 +0300

    repository

---
+++ slide3.php
@@ -0,0 +1,23 @@
+<section class="maxwidth-theme">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1">
+				<div class="col-sm-4">
+					<p class="slide3-text1 center-block text-center">АУДИТ ПРОИЗВОДИТЕЛЬНОСТИ СЕРВЕРА</p>
+					<p class="slide3-price center-block text-center">15 000</p>
+					<button class="slide3-button center-block">НАЧАТЬ РАБОТУ</button>
+				</div>
+				<div class="col-sm-4">				
+					<p class="slide3-text1 center-block text-center">РАЗОВАЯ НАСТРОЙКА СЕРВЕРА</p>
+					<p class="slide3-price center-block text-center">30 000</p>
+					<button class="slide3-button center-block">НАЧАТЬ РАБОТУ</button>
+				</div>
+				<div class="col-sm-4">			
+					<p class="slide3-text1 center-block text-center">СТОИМОСТЬ ДОП. УСЛУГ</p>
+					<p class="slide3-price center-block text-center">15 000</p>
+					<button class="slide3-button center-block">НАЧАТЬ РАБОТУ</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
