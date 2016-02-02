4c4d66dc2611e80042b35453a9693b4f3cbfb94e
marat <akhmadiev1993@mail.ru>
2016-02-02 15:07:13 +0300

    slide5slide6

---
+++ slide6.php
@@ -0,0 +1,24 @@
+<section class="maxwidth-theme">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1">
+				<p class="slide6-text1">Способы оплаты</p>
+				<div class="col-sm-4">
+					<img src="" alt="" class="center-block">
+					<p class="slide6-text2 center-block text-center">НАЛИЧНЫЙ РАСЧЕТ</p>
+					<p class="slide6-text3 center-block text-center">на наш счет в Яндекс деньгах (без комиссии в Евросети и Связном)</p>
+				</div>
+				<div class="col-sm-4">					
+					<img src="" alt="" class="center-block">
+					<p class="slide6-text2 center-block text-center">ЛЮБОЙ КАРТОЙ</p>
+					<p class="slide6-text3 center-block text-center">Необходимо зарегистрироваться у нас на сайте и пополнить счет на нужную сумму.</p>
+				</div>
+				<div class="col-sm-4">					
+					<img src="" alt="" class="center-block">
+					<p class="slide6-text2 center-block text-center">БЕЗНАЛИЧНЫЙ РАСЧЕТ</p>
+					<p class="slide6-text3 center-block text-center">По договору (без/НДС)</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
